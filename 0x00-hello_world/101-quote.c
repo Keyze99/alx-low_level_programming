ebab5ee046e36be1877a73bd442d73a903a6b145
elkhalil zbir <elkhalil.zbir@gmail.com>
2023-10-05 19:38:31 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include<unistd.h>
 
 /**
- *main - prints "and that piece of art is useful - Dora Korpar, 2015-10-19
+ *main - prints and that piece of art is useful - Dora Korpar, 2015-10-19
  *followed by new line to the standard error
- *return : always 0(Sucess)
+ *return: always 0 (Success)
  */
 int main(void)
 {
