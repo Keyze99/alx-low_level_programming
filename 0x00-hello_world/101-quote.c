6e4582fd09c81273cc811d21d983c15bf9f2ed79
elkhalil zbir <elkhalil.zbir@gmail.com>
2023-10-05 19:36:08 -0700

    fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
+
+/**
+ *main - prints "and that piece of art is useful - Dora Korpar, 2015-10-19
+ *followed by new line to the standard error
+ *return : always 0(Sucess)
+ */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
- 
-main - this function outputs the size of variables
-Return: Always 0 (Success)
+*main - this function outputs the size of variables
+*Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -11,9 +10,10 @@ int main(void)
 	long c;
 	long long d;
 	float e;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
