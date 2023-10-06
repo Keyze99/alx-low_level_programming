d002286f666c99a2a5c076382b6f1862d1b4addd
elkhalil zbir <elkhalil.zbir@gmail.com>
2023-10-05 19:41:24 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 
 /**
  *main - prints and that piece of art is useful - Dora Korpar, 2015-10-19
- *followed by new line to the standard error
- *return: always 0 (Success)
+ *followed by new line, to the standard error.
+ *return: always 1 (Success)
  */
 int main(void)
 {
