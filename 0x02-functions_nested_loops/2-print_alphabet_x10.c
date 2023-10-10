9dc96e2f11ad0fa6720baa56f5d46cf1d80d4f56
elkhalil zbir <elkhalil.zbir@gmail.com>
2023-10-10 16:53:41 -0700

    one last fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lowercase
+ * print_alphabet_x10 - prints alphabet in lowercase
  * followed by a new line 10 times
  */
 void print_alphabet_x10(void)
